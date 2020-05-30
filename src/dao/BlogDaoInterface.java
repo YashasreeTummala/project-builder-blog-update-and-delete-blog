22a94a224e107a11acc2f437370a1290c98d615b
yashasree <yashasreeaditya@gmail.com>
2020-05-31 00:14:43 +0530

    FSJ0420A105

--- src/dao/BlogDaoInterface.java
+++ src/dao/BlogDaoInterface.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import model.Blog;
 
-public interface BlogDaoInterface {
+interface BlogDaoInterface
+{
+	//Stage-4 Part-1 (Create, Read)
+	void insertBlog(Blog blog) throws SQLException, Exception;
+	List selectAllBlogs() throws SQLException, Exception;
 	
-		void insertBlog(Blog blog) throws SQLException;
-		Blog selectBlog(int blogid);
-	List<Blog> selectAllBlogs();
-		boolean deleteBlog(int id) throws SQLException;
-		boolean updateBlog(Blog blog) throws SQLException, Exception;
-	
+	//Stage-4 Part-2 (update, delete)
+	Blog selectBlog(int blogid) throws SQLException, Exception;
+	boolean deleteBlog(int id) throws SQLException, Exception;
+	boolean updateBlog(Blog blog) throws SQLException, Exception;
 }
